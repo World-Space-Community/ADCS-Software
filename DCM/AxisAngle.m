7363802a0b7a0c07989988801f39f7aed645bbbe
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2023-05-07 19:30:59 -0400

    [DCM] Test

---
+++ DCM/AxisAngle.m
@@ -0,0 +1,6 @@
+function R = AxisAngle(a, phi)
+% Construct a rotation matrix using the axis (a) and angle (phi) of rotation.
+    phi = deg2rad(phi);
+    is_column(a);
+    R = cos(phi) * eye(3) + (1 - cos(phi)) * (a * a.') - sin(phi) * uX(a);
+end
---
+++ DCM/is_column.m
@@ -0,0 +1,6 @@
+function is_column(a)
+    % Check if the input vector is a column vector.
+    if size(a, 2) ~= 1
+        error('Input vector must be a column vector');
+    end
+end
